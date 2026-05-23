9b71995c190acdf1461d4defafccff7981150f34
kokomitzu <brentottttt2005@gmail.com>
2026-05-23 21:15:21 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE = 'linedist-v6';
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(cache => Promise.all([
+        cache.add(new Request('/', { cache: 'reload' })),
+        cache.add(new Request('/index.html', { cache: 'reload' }))
+      ]))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+
+  e.respondWith(
+    caches.open(CACHE).then(cache => {
+      return cache.match('/index.html').then(cached => {
+        if (cached) return cached;
+        return fetch(e.request).then(res => {
+          if (res.ok) cache.put(e.request, res.clone());
+          return res;
+        }).catch(() => cache.match('/index.html'));
+      });
+    })
+  );
+});
